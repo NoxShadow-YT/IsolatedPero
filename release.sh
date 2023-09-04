1788c8f3de8daa341790d0d6992bec3bd268f7fa
诺影科技 NoxShadow Tech <115096818+HackerYijia@users.noreply.github.com>
2023-09-04 16:57:40 -0400

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+HOME="/home/container"
+HOMEA="$HOME/linux/.apt"
+STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
+STAR2="$HOMEA/usr/include/x86_64-linux-gnu:$HOMEA/usr/include/x86_64-linux-gnu/bits:$HOMEA/usr/include/x86_64-linux-gnu/gnu"
+STAR3="$HOMEA/usr/share/lintian/overrides/:$HOMEA/usr/src/glibc/debian/:$HOMEA/usr/src/glibc/debian/debhelper.in:$HOMEA/usr/lib/mono"
+STAR4="$HOMEA/usr/src/glibc/debian/control.in:$HOMEA/usr/lib/x86_64-linux-gnu/libcanberra-0.30:$HOMEA/usr/lib/x86_64-linux-gnu/libgtk2.0-0"
+STAR5="$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/modules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/immodules:$HOMEA/usr/lib/x86_64-linux-gnu/gtk-2.0/2.10.0/printbackends"
+STAR6="$HOMEA/usr/lib/x86_64-linux-gnu/samba/:$HOMEA/usr/lib/x86_64-linux-gnu/pulseaudio:$HOMEA/usr/lib/x86_64-linux-gnu/blas:$HOMEA/usr/lib/x86_64-linux-gnu/blis-serial"
+STAR7="$HOMEA/usr/lib/x86_64-linux-gnu/blis-openmp:$HOMEA/usr/lib/x86_64-linux-gnu/atlas:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-miners-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/tracker-2.0:$HOMEA/usr/lib/x86_64-linux-gnu/lapack:$HOMEA/usr/lib/x86_64-linux-gnu/gedit"
+STARALL="$STAR1:$STAR2:$STAR3:$STAR4:$STAR5:$STAR6:$STAR7"
+export LD_LIBRARY_PATH=$STARALL
+export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
+export BUILD_DIR=$HOMEA
+
+bold=$(echo -en "\e[1m")
+nc=$(echo -en "\e[0m")
+lightblue=$(echo -en "\e[94m")
+lightgreen=$(echo -en "\e[92m")
+
+echo "
+${bold}${lightgreen}========================================================================
+                                                                                                  
+${bold}${lightblue}@@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@      @@@  @@@  @@@@@@@@@@
+${bold}${lightblue}@@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@     @@@  @@@  @@@@@@@@@@@    
+${bold}${lightblue}@@!  @@@    @@!    @@!       @@!  @@@  @@!  @@@     @@!  @@@  @@! @@! @@!    
+${bold}${lightblue}!@!  @!@    !@!    !@!       !@!  @!@  !@!  @!@     !@!  @!@  !@! !@! !@!     
+${bold}${lightblue}@!@@!@!     @!!    @!!!:!    @!@!!@!   @!@  !@!     @!@  !@!  @!! !!@ @!@      
+${bold}${lightblue}!!@!!!      !!!    !!!!!:    !!@!@!    !@!  !!!     !@!  !!!  !@!   ! !@!        
+${bold}${lightblue}!!:         !!:    !!:       !!: :!!   !!:  !!!     :!:  !!:  !!:     !!:        
+${bold}${lightblue}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!   :!:     :!:            
+${bold}${lightblue} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
+${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :       :      :          
+                                                                                                  
+                                                                                                                
+${bold}${lightgreen}========================================================================
+"
+ 
+echo "${nc}"
+
+if [[ -f "./installed" ]]; then
+    echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
+    function runcmd1 {
+        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd
+    }
+    function runcmd {
+        printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd1
+    }
+    runcmd
+else
+    echo "Downloading files for application"
+    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
+    echo -ne '#                   (5%)\r'
+    curl -sSLo files.zip https://github.com/NoxShadow-YT/IsolatedPero/sources/release.zip >/dev/null 2>err.log
+    echo -ne '###                 (15%)\r'
+    # Add more download steps here if necessary
+
+    echo "Extracting files"
+    unzip -qq ngrok.zip
+    echo -ne '#####               (30%)\r'
+    unzip -qq files.zip
+    echo -ne '########            (45%)\r'
+    # Add more extraction steps here if necessary
+
+    echo "Setting up IsolatedPero"
+    # Add setup instructions here
+
+    touch "./installed"
+    echo "IsolatedPero installation completed"
+fi
